49de67d24b836ec221605aab1dc54e7d6bf2d847
mcarlod <diemy006@mymail.unisa.edu.au>
2024-03-16 22:10:35 +1030

    Base code for Item.java
    Added base for all functions and values.
    Added tick, setAge, died, equals method functions.

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -2,5 +2,43 @@ package students.items;
 
 public class Item {
 
+	int age;
+	int maturationAge;
+	int deathAge;
+	double monetaryValue;
 	
+	public Item(int maturationAge, int deathAge, double monetaryValue) {
+		this.age = 0;
+		this.maturationAge = maturationAge;
+		this.deathAge = deathAge;
+		this.monetaryValue = monetaryValue;
+	}
+	
+	public void tick() {
+		this.age += 1;
+	}
+	
+	public void setAge(int newAge) {
+		this.age = newAge;
+	}
+	
+	public boolean died() {
+		if (this.age > this.deathAge) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public String getValue() {
+		
+	}
+	
+	public boolean equals(Item otherItem) {
+		return this.age == otherItem.age && this.maturationAge == otherItem.maturationAge && this.deathAge == otherItem.deathAge && this.monetaryValue == otherItem.monetaryValue;
+	}
+	
+	public String toString() {
+		
+	}
 }
